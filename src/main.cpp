de2ec0f1b3939c20d1f73568bfc54788f9327b48
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2023-09-16 15:55:26 +0200

    Create test image

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,10 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 
+#include <algorithm>
 #include <cstdint>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <utility>
 #include <vector>
@@ -94,6 +96,33 @@ Scope_guard(F &&) -> Scope_guard<F>;
     return image;
 }
 
+[[nodiscard]] inline Eigen::Vector3f
+get_pixel(const Image &image, std::size_t i, std::size_t j)
+{
+    const auto pixel_index = i * image.width + j;
+    return {u8_to_float(image.pixel_data[pixel_index * 3 + 0]),
+            u8_to_float(image.pixel_data[pixel_index * 3 + 1]),
+            u8_to_float(image.pixel_data[pixel_index * 3 + 2])};
+}
+
+void create_test_image(const char *file_name)
+{
+    constexpr std::uint8_t pixels[7][13] {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 255, 0, 255, 0, 255, 255, 255, 0, 255, 0, 255, 0},
+        {0, 255, 0, 255, 0, 255, 0, 0, 0, 255, 0, 255, 0},
+        {0, 255, 255, 255, 0, 255, 255, 0, 0, 0, 255, 0, 0},
+        {0, 255, 0, 255, 0, 255, 0, 0, 0, 0, 255, 0, 0},
+        {0, 255, 0, 255, 0, 255, 255, 255, 0, 0, 255, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    constexpr std::size_t height {std::size(pixels)};
+    constexpr std::size_t width {std::size(pixels[0])};
+
+    std::ofstream file(file_name);
+    file << "P5 " << width << ' ' << height << " 255 ";
+    file.write(reinterpret_cast<const char *>(pixels), width * height);
+}
+
 inline void sdl_check(int result)
 {
     if (result != 0)
@@ -120,7 +149,7 @@ void application_main(Network &network, Eigen::VectorXf &input)
                                          SDL_WINDOWPOS_UNDEFINED,
                                          1280,
                                          720,
-                                         {});
+                                         SDL_WINDOW_RESIZABLE);
     sdl_check(window);
     DEFER([window] { SDL_DestroyWindow(window); });
 
@@ -138,6 +167,10 @@ void application_main(Network &network, Eigen::VectorXf &input)
                                            texture_height);
     sdl_check(texture);
     DEFER([texture] { SDL_DestroyTexture(texture); });
+
+    constexpr auto image_file_name {"hey.pgm"};
+    create_test_image(image_file_name);
+    const auto image = load_image(image_file_name);
 
     bool quit {false};
     while (!quit)
@@ -189,37 +222,27 @@ void application_main(Network &network, Eigen::VectorXf &input)
         sdl_check(SDL_RenderCopy(renderer, texture, nullptr, nullptr));
         SDL_RenderPresent(renderer);
 
-        constexpr int image[7][13] {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                    {0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0},
-                                    {0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0},
-                                    {0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0},
-                                    {0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0},
-                                    {0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0},
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
-        constexpr std::size_t rows {std::size(image)};
-        constexpr std::size_t cols {std::size(image[0])};
-
-        for (std::size_t i {0}; i < rows; ++i)
+        float total_loss {0.0f};
+        const float learning_rate {0.005f};
+        for (std::size_t i {0}; i < image.height; ++i)
         {
-            for (std::size_t j {0}; j < cols; ++j)
+            for (std::size_t j {0}; j < image.width; ++j)
             {
                 const auto x = static_cast<float>(j) /
-                                   static_cast<float>(cols - 1) * 2.0f -
+                                   static_cast<float>(image.width - 1) * 2.0f -
                                1.0f;
                 const auto y = static_cast<float>(i) /
-                                   static_cast<float>(rows - 1) * 2.0f -
+                                   static_cast<float>(image.height - 1) * 2.0f -
                                1.0f;
-                const float learning_rate {0.002f};
                 input.setZero();
                 input << x, y;
+                const auto output = get_pixel(image, i, j);
                 stochastic_gradient_descent(
-                    network,
-                    input,
-                    image[i][j] ? Eigen::Vector3f(0.9f, 0.9f, 0.9f)
-                                : Eigen::Vector3f(-0.9f, -0.9f, -0.9f),
-                    learning_rate);
+                    network, input, output, learning_rate);
+                total_loss += loss(network, output);
             }
         }
+        std::cout << total_loss << '\n';
     }
 }
 
@@ -230,7 +253,7 @@ int main()
     try
     {
         Network network {};
-        init_network(network, {2, 40, 20, 20});
+        init_network(network, {2, 40, 20});
         Eigen::VectorXf input(2);
 
         Eigen::internal::set_is_malloc_allowed(false);
